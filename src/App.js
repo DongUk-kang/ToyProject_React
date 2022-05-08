33b54b443cdf5a4f0f6ec8a5f908276b3e28f0e3
soobing <qls0147@naver.com>
2022-05-08 18:00:23 +0900

    fix(webpack): css load, asset module

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { mainWrapper } from 'App.css';
-import React from 'react';
-import { hot } from 'react-hot-loader';
+import { mainWrapper } from "./App.css";
+import React from "react";
+import { hot } from "react-hot-loader";
+import appleImage from "../src/images/apple.jpeg";
 
 const App = () => {
-    return (
-        <div className={mainWrapper}>
-            <h1>Hello, World</h1>
-        </div>
-    )
-}
+  return (
+    <div className={mainWrapper}>
+      <h1>Hello, World</h1>
+      <img src={appleImage} />
+    </div>
+  );
+};
 
 export default hot(module)(App);
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,60 @@
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const port = 3000;
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.[hash].js',
-        publicPath: '/'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/dist",
+    filename: "bundle.[hash].js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+            options: {
+              esModule: false,
             },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                            localsConvention: 'camelCase',
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new HtmlWebpackPlugin({
-            template: 'public/index.html',
-        })
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                exportOnlyLocals: true,
+                exportLocalsConvention: "camelCase",
+              },
+            },
+          },
+        ],
+      },
     ],
-    devServer: {
-        host: 'localhost',
-        port: port,
-        open: true,
-        historyApiFallback: true,
-        hot: true,
-    },
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
+  devServer: {
+    host: "localhost",
+    port: port,
+    open: true,
+    historyApiFallback: true,
+    hot: true,
+  },
 };
